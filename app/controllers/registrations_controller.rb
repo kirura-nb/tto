0c2e02d40704fcbf7ebcb6e8804198766154c4d8
unknown <JR2353@bijisen.local>
2025-01-30 10:29:41 +0900

    ログイン機能作成中

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,22 +1,35 @@
 class RegistrationsController < ApplicationController
-    def new
-    end
-  
-    def create
-      # 簡易的な例です。本番では適切なバリデーションを追加してください。
-      @name = params[:name]
-      @department = params[:department]
-      @student_id = params[:student_id]
-      @password = params[:password]
-      @password_confirmation = params[:password_confirmation]
-  
-      if @password == @password_confirmation && @password.length.between?(8, 16)
-        flash[:notice] = "登録が成功しました"
-        redirect_to action: :new
-      else
-        flash[:alert] = "登録に失敗しました"
-        render :new
-      end
-    end
+  def new
+    @user_new = User.new
+    # 学科プルダウンメニュー
+    @departments = Department.pluck(:department, :id)
   end
+
+  def edit
+
+  end
+
+  def create
+      @user_new = User.new(user_params)
+
+      if @user_new.save
+          session[:department] = @user_new.department
+          flash[:notice] = "アカウントを作成しました。"
+          redirect_to index_list_path
+      else
+          flash.now[:alert] = "アカウント作成に失敗しました。"
+          render action: "new"
+      end
+  end
+
+  def update
+
+  end
+
+  private
+
+  def user_params
+      params.require(:user).permit(:name, :department, :number, :password)
+  end
+end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
-  root :to =>"list#index"
+  # root :to =>'list#index'
+  root :to =>'sessions#new'
+
+  # 新規登録画面パス
+  resource :registrations, only: [:new, :edit, :create]
+
+  # ログインパス
+  get 'login',  to: 'sessions#new', as: 'new_sessions'
+  post 'login', to: 'sessions#create', as: 'create_sessions'
+  delete 'logout', to: 'sessions#destroy', as: 'destroy_sessions'
+
+  # 試験パス
   get 'list/index', to: 'list#index', as: 'index_list'
   get 'test/play', to: 'test#play', as: 'play_test'
   get 'test/check', to: 'test#check', as: 'check_test'
   post 'editing/new', to: 'editing#new', as: 'new_editing'
   post 'editing/edit', to: 'editing#edit', as: 'edit_editing'
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "registrations/new"
-  get "sessions/new"
 end
